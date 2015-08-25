ae161fe895cf83a65d25cb44e8847ebc5e0f24e4
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-08-25 17:21:27 -0500

    added serial base mixin and updated depends

---
+++ chert/sqlalchemy.py
@@ -0,0 +1,18 @@
+
+class SerialBase(object):
+    def serialize(self):
+        data = dict()
+        table = self.__table__
+        for column in table.columns:
+            name = column.name
+            try:
+                pytype = column.type.python_type
+            except NotImplementedError:
+                print "NOTIMPLEMENTEDERROR", column.type
+            value = getattr(self, name)
+            if pytype is datetime or pytype is date:
+                if value is not None:
+                    value = value.isoformat()
+            data[name] = value
+        return data
+    
