2e9c9650e0dcea341777c9ac887f8b1d9a26c5ef
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-12-04 18:09:57 -0600

    added  config module

---
+++ chert/config.py
@@ -0,0 +1,21 @@
+import os
+from ConfigParser import ConfigParser
+from StringIO import StringIO
+
+
+DEFAULT_CONFIG_TEXT = """\
+# Configuration file for chert
+
+[main]
+# put something here....
+"""
+
+default_file = os.path.expanduser('~/.config/chert/chert.ini')
+config = ConfigParser()
+if not os.path.isfile(default_file):
+    print 'Generating default config...'
+    config.readfp(StringIO(DEFAULT_CONFIG_TEXT))
+    with file(default_file, 'w') as outfile:
+        config.write(outfile)
+
+config.read([default_file])
