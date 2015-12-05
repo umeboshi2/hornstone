9c51933439c21a5ae9db3be10756539a4c8f1b50
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-12-04 18:12:49 -0600

    make sure to create directories

--- chert/config.py
+++ chert/config.py
@@ -13,6 +13,9 @@ DEFAULT_CONFIG_TEXT = """\
 default_file = os.path.expanduser('~/.config/chert/chert.ini')
 config = ConfigParser()
 if not os.path.isfile(default_file):
+    dirname = os.path.dirname(default_file)
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
     print 'Generating default config...'
     config.readfp(StringIO(DEFAULT_CONFIG_TEXT))
     with file(default_file, 'w') as outfile:
