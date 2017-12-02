4a9fcf626b9b650adf663a0b8b621f08f5dfad32
Joseph Rawson <joseph.rawson.works@gmail.com>
2017-12-02 10:30:46 -0600

    updated setup

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ version = '0.0'
 requires = [
     'SQLAlchemy',
     'psycopg2',        # dbapi for postgresql
+    'robobrowser',
     'requests',
     'beautifulsoup4',
     'lxml',
-    'mechanize',
     'transaction',     # I am not sure if I should use this or not
     'rarfile',
     'unipath',
@@ -25,7 +25,7 @@ A bunch of rocks""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Joseph Rawson',
-      author_email='joseph.rawson.works@littledebian.org',
+      author_email='joseph.rawson.works@gmail.com',
       url='https://github.com/umeboshi2/chert',
       license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
@@ -35,8 +35,5 @@ A bunch of rocks""",
       entry_points="""
       # -*- Entry points: -*-
       [console_scripts]
-      #tmbnex-update-repos = chert.scripts.tmbnex_update_repos:main
-      #tmbnex-create-annex = chert.scripts.tmbnex_create_annex:main
-      #update-from-cypress = chert.scripts.update_from_cypress:main
       """,
       )
