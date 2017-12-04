07cd544ccb86142eef3a9e6be2a972f80cf3ed8f
Joseph Rawson <joseph.rawson.works@gmail.com>
2017-12-03 21:51:50 -0600

    fix configparser usage

--- chert/scummvm/runproc.py
+++ chert/scummvm/runproc.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-from configparser import ConfigParser
+from configparser import ConfigParser, NoSectionError
 
 main_config = ConfigParser()
 main_config.read('config/main.ini')
@@ -8,7 +8,10 @@ main_config.read('config/main.ini')
 scummvm_config = ConfigParser()
 scummvm_config.read('config/scummvm.ini')
 
-add_sdl_var = scummvm_config.getboolean('main', 'add_sdl_environ_var')
+add_sdl_var = False
+if 'main' in scummvm_config.sections():
+    cfgopt = 'add_sdl_environ_var'
+    add_sdl_var = scummvm_config['main'].getboolean(cfgopt, False)
 if add_sdl_var:
     os.environ['SDL_MOUSE_RELATIVE'] = '0'
 
@@ -16,8 +19,11 @@ if add_sdl_var:
 def build_cmd(directory):
     mdata = scummvm_config.items('main')
     cmd = [mdata['scummvm_bin'], '-e', mdata['music_driver']]
-    if scummvm_config.getboolean('main', 'full_screen'):
-        cmd.append('-f')
+    try:
+        if scummvm_config.getboolean('main', 'full_screen'):
+            cmd.append('-f')
+    except NoSectionError:
+        pass
     cmd += ['-g', mdata['graphics_scaler']]
     cmd += ['-p', directory]
     return cmd
