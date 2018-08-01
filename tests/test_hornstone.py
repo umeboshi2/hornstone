8816e635718eb159bb00a598a61ce705fb86ecdd
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-08-01 11:15:52 -0500

    added more tests

--- tests/test_hornstone.py
+++ tests/test_hornstone.py
@@ -3,9 +3,13 @@
 
 """Tests for `hornstone` package."""
 
+import io
 import pytest
 
 import hornstone
+from hornstone.base import get_sha256sum, get_sha256sum_string
+from hornstone.base import trailing_slash, remove_trailing_slash
+
 
 
 @pytest.fixture
@@ -22,3 +26,48 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+
+hello_hash = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+
+
+def test_hash_hello_file():
+    hfile = io.BytesIO(b'hello')
+    fhash = get_sha256sum(hfile)
+    assert fhash == hello_hash
+
+
+def test_hash_hello_string():
+    hello = b'hello'
+    fhash = get_sha256sum_string(hello)
+    assert fhash == hello_hash
+
+def test_trailing_slash():
+    u = 'hello/'
+    res = trailing_slash(u)
+    assert res == u
+
+def test_trailing_slash_needed():
+    u = 'hello'
+    res = trailing_slash(u)
+    assert res == 'hello/'
+
+
+def test_rm_slash1():
+    u = "end/"
+    res = remove_trailing_slash(u)
+    assert res == "end"
+
+
+def test_rm_slash2():
+    u = "end"
+    res = remove_trailing_slash(u)
+    assert res == "end"
+
+def test_rm_slash3():
+    u = "end/////"
+    res = remove_trailing_slash(u)
+    assert res == "end"
+
+
+
