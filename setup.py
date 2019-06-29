5372cddd3cf4872b4b4e9f42a672dd6fdfa7773a
Joseph Rawson <joseph.rawson.works@gmail.com>
2019-06-29 11:37:45 -0500

    Bump version: 0.1.15 → 0.1.16

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ test_requirements = [
 
 setup(
     name='hornstone',
-    version='0.1.15',
+    version='0.1.16',
     description="A bunch of rocks",
     long_description=readme,
     author="Joseph Rawson",
